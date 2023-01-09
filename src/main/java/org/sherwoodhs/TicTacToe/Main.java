77c0ab7f0fbfe7957888d8c634548b5764c428aa
MiddlePedal <christopherip23@sherwoodsd.com>
2023-01-09 09:25:54 -0800

    Added a folder for TicTacToe

---
+++ src/main/java/org/sherwoodhs/TicTacToe/Main.java
@@ -0,0 +1,15 @@
+package org.sherwoodhs.TicTacToe;
+
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.logging.Logger;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Game();
+    }
+
+}
