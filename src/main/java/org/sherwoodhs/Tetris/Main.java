1d0358e52538aad9ee884e10706ab1fb066c1e1c
hangilk <114253929+hangilk@users.noreply.github.com>
2023-01-12 22:31:07 -0800

    Update Main.java

--- src/main/java/org/sherwoodhs/Tetris/Main.java
+++ src/main/java/org/sherwoodhs/Tetris/Main.java
@@ -1,7 +1,34 @@
 package org.sherwoodhs.Tetris;
 
-public class Main {
+import java.awt.EventQueue;
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Main extends JFrame {
+    private JLabel status;
+    public Main() {
+        createUI();
+    }
+    public void createUI() {
+        setTitle("Tetris");
+        setSize(200, 400);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        status = new JLabel("Lines: 0");
+        add(status, BorderLayout.SOUTH);
+        Game game = new Game(this);
+        add(game);
+        game.start();
+    }
+    public JLabel getStatus() {
+        return status;
+    }
     public static void main(String[] args) {
-        System.out.print();
+        EventQueue.invokeLater(() -> {
+            Main tetris = new Main();
+            tetris.setVisible(true);
+        });
     }
 }
