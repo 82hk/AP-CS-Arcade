836dd5a8406e801357e2eac96261ec5dff02afe8
hangilk <114253929+hangilk@users.noreply.github.com>
2023-01-12 22:27:39 -0800

    Update Piece.java

--- src/main/java/org/sherwoodhs/Tetris/Piece.java
+++ src/main/java/org/sherwoodhs/Tetris/Piece.java
@@ -30,4 +30,46 @@ public class Piece {
         }
         this.piece = piece;
     }
+    private void setX(int index, int x) {
+        coords[index][0] = x;
+    }
+    private void setY(int index, int y) {
+        coords[index][1] = y;
+    }
+    public int getX(int index) {
+        return coords[index][0];
+    }
+    public int getY(int index) {
+        return coords[index][1];
+    }
+    public Tetromino getPiece() {
+        return piece;
+    }
+    // change this to a better system in the future
+    public void setRandomPiece() {
+        Random r = new Random();
+        int x = Math.abs(r.nextInt()) % 7 + 1;
+        Tetromino[] values = Tetromino.values();
+        setPiece(values[x]);
+    }
+    public int minY() {
+        int y = coords[0][1];
+        for (int i = 0; i < 4; i++) {
+            y = Math.min(y, coords[i][1]);
+        }
+        return y;
+    }
+    // rotates the piece counter-clockwise if applicable
+    Piece rotate() {
+        if (piece == Tetromino.O_MINO) {
+            return this;
+        }
+        var res = new Piece();
+        res.piece = piece;
+        for (int i = 0; i < 4; i++) {
+            res.setX(i, -getY(i));
+            res.setY(i, getX(i));
+        }
+        return res;
+    }
 }
