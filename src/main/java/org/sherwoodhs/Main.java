3abf2827cad66d55dea59d28f3c047705771f3b5
landonsm23 <landonsmalan@gmail.com>
2023-01-18 12:01:04 -0800

    edited word list in wordle

--- src/main/java/org/sherwoodhs/Main.java
+++ src/main/java/org/sherwoodhs/Main.java
@@ -2,6 +2,6 @@ package org.sherwoodhs;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("I LOVE GAMES!");
     }
 }
