220e6b9e06beff1e29b4b798068f991289c1dfa7
Chris <114255366+MiddlePedal@users.noreply.github.com>
2023-01-19 14:08:06 -0800

    Update TestConnect4.java

--- src/main/java/org/sherwoodhs/Connect4/TestConnect4.java
+++ src/main/java/org/sherwoodhs/Connect4/TestConnect4.java
@@ -1,20 +1,28 @@
 package org.sherwoodhs.Connect4;
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
 public class TestConnect4 implements ActionListener {
-    private JFrame frame;
-    private JPanel panel;
-    private JButton[] button;
-    private int[][] board;
-    private int currentPlayer;
 
-    public TestConnect4() {
-        System.out.println("Test");
+    private void DrawWindow() {
+        
     }
+    
+    private void ResetGame() {
+        
+    }
+    
+    private void CheckWin() {
+        
+    }
+    
+    private void ClaimSpot() {
 
-    private void checkWin() {
+    }
+    
+    public TestConnect4() {
 
     }
 
