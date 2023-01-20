00b5fc415a27de8bab5d1a5ff0f0e07b14c27ba0
Archnitest <114258594+Archnitest@users.noreply.github.com>
2023-01-20 15:07:38 -0800

    Quality of Life Fix

--- src/main/java/org/sherwoodhs/CardsOfFate/Entities/Player.java
+++ src/main/java/org/sherwoodhs/CardsOfFate/Entities/Player.java
@@ -8,7 +8,7 @@ public class Player extends Deck {
     private static Card[] decks = {Fool.getInstance(), Fortune.getInstance(), new Swords(1), new Swords(2), new Wands(1), new Wands(2), new Coins(1), new Coins(2), new Goblets(1), new Goblets(2)};
     private static Player player = new Player("Player", decks);
     private Player (String name, Card[] deck){
-        super (name,1, deck);
+        super (name,30, deck);
     }
     public static Player getInstance () {
         return (player);
