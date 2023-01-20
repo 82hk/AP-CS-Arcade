c2dab636f56219d1a20b2a6be4f9ebbc2ded207e
Archnitest <114258594+Archnitest@users.noreply.github.com>
2023-01-20 15:07:58 -0800

    Quality of Life Fix

--- src/main/java/org/sherwoodhs/CardsOfFate/Entities/Dummy.java
+++ src/main/java/org/sherwoodhs/CardsOfFate/Entities/Dummy.java
@@ -2,6 +2,6 @@ package org.sherwoodhs.CardsOfFate.Entities;
 
 public class Dummy extends Enemy{
     public Dummy (){
-        super("Dummy", 1, 0, 0,0,0);
+        super("Dummy", 1, 0, 1,0,1);
     }
 }
