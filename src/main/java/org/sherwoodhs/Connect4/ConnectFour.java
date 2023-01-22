64b1e90c6097460aa644a42170653424220d8ca4
Chris <114255366+MiddlePedal@users.noreply.github.com>
2023-01-21 22:16:08 -0800

    Update ConnectFour.java

--- src/main/java/org/sherwoodhs/Connect4/ConnectFour.java
+++ src/main/java/org/sherwoodhs/Connect4/ConnectFour.java
@@ -1,37 +1,24 @@
-package org.sherwoodhs.Connect4;
-
 import java.awt.*;
+import java.util.*;
 import java.awt.event.*;
 import javax.swing.*;
 import static javax.swing.JFrame.*;
 
 /*
-
 Class is no longer a JFrame itself, but has a JFrame component
-
 Class no longer contains main method
-
 Program is run through separate main class by calling (initializing?) TestConnect4 class method
-
 Added reset & exit buttons with methods
-
 Button claiming is now disabled after a player wins, until the game is reset
-
 Added boolean isWin for if player has won
-
 Separated ActionListener code from Button Initializer (iterator) code.
 ActionListener code can be found at bottom of class.
-
 Divided up program into distinct methods
-
 ----------------------------------------------------------
-
 NOTE: Buttons visually clear, but remain claimed even after pressing reset.
       Need to figure out button claiming logic.
-
       Also buttons move down *kind of*
       Need to do more testing to figure out how/why
-
  */
 
 public class ConnectFour implements ActionListener {
@@ -43,6 +30,8 @@ public class ConnectFour implements ActionListener {
     private JButton exitButton;
     private JButton[] button;
     private boolean isWin;
+    private int column;
+    private int k;
     private int[][] board; // 2D array
     private int currentPlayer;
 
@@ -77,12 +66,18 @@ public class ConnectFour implements ActionListener {
         frame.setLayout(new BorderLayout());
 
         panel.setLayout(new GridLayout(6,7));
+        panel.setVisible(true);
 
         for (int i = 0; i < 42; i++) {
             button[i] = new JButton();
             button[i].addActionListener(this::actionPerformed);
+            button[i].setVisible(true);
+          
             // separated the ActionListener code (at bottom) from the button iterator
             panel.add(button[i]);
+        }
+        for (int i = 7; i < 42; i++) {
+          button[i].setEnabled(false);
         }
 
         frame.add(title, BorderLayout.NORTH);
@@ -93,6 +88,9 @@ public class ConnectFour implements ActionListener {
         for (int i = 0; i < 42; i++) {
             button[i].setBackground(new JButton().getBackground());
             button[i].setEnabled(true);
+        }
+        for (int i = 7; i < 42; i++) {
+          button[i].setEnabled(false);
         }
         for (int x = 5; x >= 0; x--){
             for (int y = 6; y >= 0; y--){
@@ -107,7 +105,7 @@ public class ConnectFour implements ActionListener {
     }
     
     private boolean CheckWin(int row, int column) {
-// check horizontal
+// check horizontal - WORKS
         int count = 1;
         for (int i = column - 1; i >= 0; i--) {
             if (board[row][i] == currentPlayer) {
@@ -126,7 +124,7 @@ public class ConnectFour implements ActionListener {
         if (count >= 4) {
             return true;
         }
-        // check vertical
+        // check vertical - DOES NOT WORK
         count = 1;
         for (int i = row - 1; i >= 0; i--) {
             if (board[i][column] == currentPlayer) {
@@ -139,7 +137,7 @@ public class ConnectFour implements ActionListener {
             return true;
         }
 
-        // check diagonal (top-left to bottom-right)
+        // check diagonal (top-left to bottom-right) - DOES NOT WORK
         count = 1;
         for (int i = 1; i < 7; i++) {
             int r = row + i;
@@ -166,7 +164,19 @@ public class ConnectFour implements ActionListener {
     }
     
     private void ClaimSpot() {
+    }
 
+    private void UpdateBoard() {
+      for (int row = 0; row < 5; row++) {
+        for (int column = 0; column < 7; column ++) {
+
+          if (board[row+1][column] == 0 && board[row][column] == currentPlayer) {
+            board[row+1][column] = currentPlayer;
+            board[row][column] = 0;
+          }
+          
+        }         
+      }
     }
     
     public ConnectFour() {
@@ -184,16 +194,28 @@ public class ConnectFour implements ActionListener {
         } else if (isWin == false) {
 
             JButton b = (JButton) e.getSource();
-            int column = -1;
+            column = -1;
 
+            k = 0;
             for (int j = 0; j < 42; j++) { // column break
                 if (b == button[j]) {
+                    k = j;
                     column = j % 7;
                     break;
                 }
-
             }
+          
+            for (int row = 0; row < 5; row++) { // can only drop from top row
+              if (board[row+1][column] == 0) {
+                k+=7; // K isn't stopping below 42.
+              } else {
+                break;
+              }
+            }
+            b = button[k]; // ERROR IS HAPPENING HERE. K TOO BIG.
+          
             if (column != -1) {
+              System.out.println("\n\n\nhere");
                 for (int row = 5; row >= 0; row--) {
                     if (board[row][column] == 0) {
                         board[row][column] = currentPlayer;
@@ -210,8 +232,9 @@ public class ConnectFour implements ActionListener {
                 }
             }
         }
+
+      //UpdateBoard();
         // below is purely for debugging purposes; prints out the 'board' 2D array
-        System.out.println("\n\n\n");
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 7; j++) {
                 System.out.print(board[i][j] + " ");
