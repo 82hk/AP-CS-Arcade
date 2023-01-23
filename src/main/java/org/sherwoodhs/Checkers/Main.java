12a58272650055f38ae63617151ab32c0f8bc009
hangilk <hangilxk24@sherwoodsd.com>
2023-01-23 12:15:15 -0800

    Checkers metadata added

--- src/main/java/org/sherwoodhs/Checkers/Main.java
+++ src/main/java/org/sherwoodhs/Checkers/Main.java
@@ -1,9 +1,39 @@
 package org.sherwoodhs.Checkers;
 
-public class Main {
+import org.sherwoodhs.Game;
+
+import java.awt.image.BufferedImage;
+
+public class Main implements Game {
 
     public static void main(String args[]) {
         CheckersGame game = new CheckersGame();
         game.start();
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public String getName() {
+        return "Checkers";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Enter your description here.";
+    }
+
+    @Override
+    public BufferedImage getThumbnail() {
+        final String path = "/checkers.png";
+        return checkThumbnail(path);
     }
 }
